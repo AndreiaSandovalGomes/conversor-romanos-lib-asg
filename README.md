d506bd8a281fc48aff681fc4a79897f807ce6412
AndreiaSandovalGomes <gomesandreia27@outlook.com>
2018-09-06 10:03:07 -0300

    identação e testes implementados e finalizados

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+DOCUMENTAÇÃO :
+
 Conversor de números romanos para inteiros e vice-versa v.1.0.0
 Esta biblioteca se destina à conversao de inteiros para romanos e vice-versa, para uso em aplicações web. Na versão atual é capaz de retornar 'V' para imput (5) ,por exemplo, e retornar 10 para imput ('X')
 
@@ -28,3 +30,20 @@ versão 3.0.0 (sem previsão ainda, aceita-se sugestões de melhorias através d
 
 versão 1.0.0 (released) e versão 2.0.0 (released)
 funcionalidades: conversão de números inteiros para romanos e vice-versa;
+
+As regras para criação dos números romanos;
+
+Um número maior à frente de um número menor significa subtração. A adição é caracterizada por um número menor à frente de um número maior. Por exemplo, IV significa 4, VI significa 6.
+
+Não se utiliza mais de um número menor antes de um número maior para subtrair. Por exemplo, IIV não corresponde a 3.
+
+Devem separar-se, dezenas, centenas, e milhares como numerais separados. Isso significa que a 99 corresponde XCIX (90 + 9), nunca deve ser escrito como IC. Similarmente, 999 não pode ser IM e 1999 não pode ser MIM.
+
+I	Corresponde ao numeral 1. II são dois, III são três, IV são 4 (ocasionalmente pode ver-se IIII como 4)
+V	Corresponde ao numeral 5. IV são 4, VI são 6, VII são 7, VIII são 8.
+X	Corresponde ao numeral 10. IX são 9, XI é 11, etc..
+L	Corresponde ao numeral 50. XL é o 40.
+C	Corresponde ao numeral 100. C tem origem na palavra latina Centum.
+D	Corresponde ao numeral 500.
+M 	Corresponde ao numeral 1000.
+Uma linha sobre um numeral significa multiplicá-lo por 1000. O numeral V com uma linha sobre o símbolo significa 5000.
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,23 @@
-const mocha = require("mocha");
-const chai = require("chai");
-const lib = require("../lib/index");
+const mocha = require('mocha');
+const chai = require('chai');
+const lib = require('../lib/index');
 const expect = chai.expect;
 
-describe("lib", () => {
-  describe("#intToRoman", () => {
-    describe("quando o valor digitado for um inteiro ", () => {
-      it("deve me retornar um numero romano", () => {
-        expect(lib.intToRoman(5)).to.equal("V");
+describe('lib', () => {
+  describe('#intToRoman', () => {
+    describe('quando o valor digitado for um inteiro ', () => {
+      it('deve me retornar um numero romano', () => {
+        expect(lib.intToRoman(1)).to.equal('I');
+      });
+    });
+  });
+});
+
+describe('lib', () => {
+  describe('#intToRoman', () => {
+    describe('quando o valor digitado for um inteiro ', () => {
+      it('deve me retornar um numero romano', () => {
+        expect(lib.intToRoman(5)).to.equal('V');
       });
     });
   });
@@ -27,17 +37,41 @@ describe('lib', () => {
   describe('intToRoman', () => {
     describe('Quando não for digitado', () => {
       it('Deve me retornar um erro', () => {
-        expect(() => lib.intToRoman()).to.throw('O campo precisa ser preenchido');
+        expect(() => lib.intToRoman(5000)).to.throw('O campo precisa ser preenchido com números até 4999');
       });
     });
   });
 });
 
-describe("lib", () => {
-  describe("#romanToInt", () => {
-    describe("quando o numero digitado for um numero romano", () => {
-      it("deve me retornar o numero romano", () => {
+describe('lib', () => {
+  describe('#romanToInt', () => {
+    describe('quando o numero digitado for um numero romano', () => {
+      it('deve me retornar o numero inteiro', () => {
+        expect(lib.romanToInt('I')).to.equal(1);
+      });
+      it('deve me retornar o numero inteiro', () => {
         expect(lib.romanToInt('V')).to.equal(5);
+      });
+      it('deve me retornar o numero inteiro', () => {
+        expect(lib.romanToInt('IX')).to.equal(9);
+      });
+      it('deve me retornar o numero inteiro', () => {
+        expect(lib.romanToInt('X')).to.equal(10);
+      });
+      it('deve me retornar o numero inteiro', () => {
+        expect(lib.romanToInt('XL')).to.equal(40);
+      });
+      it('deve me retornar o numero inteiro', () => {
+        expect(lib.romanToInt('L')).to.equal(50);
+      });
+      it('deve me retornar o numero inteiro', () => {
+        expect(lib.romanToInt('C')).to.equal(100);
+      });
+      it('deve me retornar o numero inteiro', () => {
+        expect(lib.romanToInt('D')).to.equal(500);
+      });
+      it('deve me retornar o numero inteiro', () => {
+        expect(lib.romanToInt('M')).to.equal(1000);
       });
     });
   });
